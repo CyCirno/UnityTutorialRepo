c666cfa48f6cb1e376087baf9e7d2d00e65a94ef
Cy <cyboy303@gmail.com>
2023-09-20 13:01:06 +0100

    Made a Test Script with debug.log and translation.

---
+++ Unity-Tutorial-Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ever on and on");
+        Debug.Log("I continue circling");
+        Debug.Log("With nothing but my hate");
+        Debug.Log("In a carousel of agony");
+
+        transform.Translate(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I love Yoomers");
+        transform.Translate(0, 0, 0);
+    }
+}
