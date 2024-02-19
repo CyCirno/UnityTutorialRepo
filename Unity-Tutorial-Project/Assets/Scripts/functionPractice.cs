4fff8db7044e0a97559c4c6b68bd3a701b4657c4
Cy <144230745+CyCirno@users.noreply.github.com>
2024-02-19 15:28:56 +0000

    Functions Stuff

---
+++ Unity-Tutorial-Project/Assets/Scripts/functionPractice.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functionPractice : MonoBehaviour
+{
+
+    void FuctionName(string Doge = "Woof woof")
+    {
+        Debug.Log(Doge);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FuctionName("woof");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//if you are reading this you are cringe and NOT based
+//if you like beastars you ARE a furry and you can't keep denying it
